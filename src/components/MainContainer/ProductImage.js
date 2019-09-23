0f4ee2c8a6c2d82a39e790c43719c2986d4c05e5
bilalsevis <bilalresmi@gmail.com>
2019-09-23 17:52:15 +0300

    Commit

--- src/components/MainContainer/ProductImage.js
+++ src/components/MainContainer/ProductImage.js
@@ -5,9 +5,9 @@ import Image from './classic.png';
 class ProductImage extends Component {
     render() {
         return (
-            <div className="col-lg-6 mb-4 product-thumbnail">
-                <div className="product-name">
-                    <h2>Original</h2>
+            <div className="col-lg-6 mb-2 product-thumbnail">
+                <div className="product-name mb-4 mt-4 text-center">
+                    <h2>Original.</h2>
                 </div>
                 <div className="product-img-box mb-4 mt-5">
                     <img src={Image} alt="" className="product-img img-fluid"/>
--- src/components/MainContainer/ProductShop.js
+++ src/components/MainContainer/ProductShop.js
@@ -7,7 +7,29 @@ class ProductShop extends Component {
             <div className="col-lg-6 col-md-6 mb-4 product-shop">
                 <div className="product-right mb-4">
                     <div className="product-name">
-                        <h1>Original</h1>
+                        <h1>Original.</h1>
+                    </div>
+                    {/* <div className="price-info">
+                        <div className="price-box">
+                            <h3>$5.00</h3>
+                        </div>
+                    </div> */}
+                    <div className="product-info mt-4">
+                        <h5 className="product-info-text">Get a special name or personal message on a Coke bottle. 
+                        Great for gifting, birthdays/holidays, weddings, 
+                        corporate events, party favors, and more.
+                        </h5>
+                    </div>
+                    <div className="product-options mt-4">
+                        <form>
+                            <div className="form-group">
+                                <input 
+                                type="text" 
+                                className="form-control"
+                                placeholder="Enter any name up to 18 characters"
+                                />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
