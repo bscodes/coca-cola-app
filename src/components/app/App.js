8639f18600dbca698202496abc29cc4351e26a84
Bilal Sevinc <bilalresmi@gmail.com>
2019-10-05 05:22:59 +0300

    Finishing Touches

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Navbar from '../Navbar/Navbar';
+import Footer from '../footer/footer';
 import MainContainer from '../MainContainer/MainContainer'
 
 function App() {
@@ -10,22 +11,11 @@ function App() {
         <Router>
             <Navbar/>
             <MainContainer />
+            <Footer/>
         </Router>
     </div>
   );
 }
 
-/* 
-önce git add . (nokta bir boşluktan sonra) ile tüm değişikleri git'e tanıtıyoruz
-sonra git commit -a ile tüm değişiklikleri local repoya ekliyoruz
-sonra git push origin master yazıp tamamını uzaktaki master'a yolluyoruz
-
-git pull origin master yazarak uzaktaki değişiklikleri projeye indiriyoruz
-
-ayrıntılar ibu sitede "https://medium.com/kodcular/k%C4%B1saca-git-ba0877d56663 ve
-https://rogerdudler.github.io/git-guide/index.tr.html"
-
-
-*/
 
 export default App;
