19ae097bd71bfff77bd6a463307b9446f851cd7a
bilalsevis <bilalresmi@gmail.com>
2019-10-07 12:43:12 +0300

    Updates

---
+++ precache-manifest.18df46c510d0e4dd6174af05bb39f65e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafcdb9e94297af672b1cf75c4c8ee69",
+    "url": "/coca-cola-app/index.html"
+  },
+  {
+    "revision": "4e9784d8612df450568c",
+    "url": "/coca-cola-app/static/css/main.31452b17.chunk.css"
+  },
+  {
+    "revision": "f1986e6abc8c38bff2f6",
+    "url": "/coca-cola-app/static/js/2.fbc84cd8.chunk.js"
+  },
+  {
+    "revision": "4e9784d8612df450568c",
+    "url": "/coca-cola-app/static/js/main.9cf5ab98.chunk.js"
+  },
+  {
+    "revision": "ddac2655f8a473390940",
+    "url": "/coca-cola-app/static/js/runtime~main.a8a74586.js"
+  },
+  {
+    "revision": "b818fbbe08a10d1839783b1e7033008d",
+    "url": "/coca-cola-app/static/media/classic.b818fbbe.png"
+  },
+  {
+    "revision": "719fa52e5f63b4bcc611f814a6e309ae",
+    "url": "/coca-cola-app/static/media/logo.719fa52e.svg"
+  },
+  {
+    "revision": "3e2860a2bcad2594372bfb612f0fa8e0",
+    "url": "/coca-cola-app/static/media/you2013bold.3e2860a2.ttf"
+  }
+]);
