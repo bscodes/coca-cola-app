0b86e713d91ff0ef12873a1faa386e5ef01acfef
bilalsevis <bilalresmi@gmail.com>
2019-10-07 13:21:13 +0300

    build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -15,17 +15,4 @@ function App() {
   );
 }
 
-/* 
-önce git add . (nokta bir boşluktan sonra) ile tüm değişikleri git'e tanıtıyoruz
-sonra git commit -a ile tüm değişiklikleri local repoya ekliyoruz
-sonra git push origin master yazıp tamamını uzaktaki master'a yolluyoruz
-
-git pull origin master yazarak uzaktaki değişiklikleri projeye indiriyoruz
-
-ayrıntılar ibu sitede "https://medium.com/kodcular/k%C4%B1saca-git-ba0877d56663 ve
-https://rogerdudler.github.io/git-guide/index.tr.html"
-
-
-*/
-
 export default App;
